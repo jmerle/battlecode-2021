c97b21ca36a516a7106af5a32c0a893e6bcbec59
Jasper van Merle <jaspervmerle@gmail.com>
2021-01-08 17:40:59 +0100

    Improve parsing boundaries

--- src/camel_case/util/MapInfo.java
+++ src/camel_case/util/MapInfo.java
@@ -22,23 +22,31 @@ public class MapInfo {
   }
 
   public static MapInfo parseBoundaries(int minX, int maxX, int minY, int maxY) {
-    if (maxX == -1 || maxY == -1) {
-      return null;
-    }
+    int size = -1;
+    int xOffset = -1;
+    int yOffset = -1;
 
-    if (minX == -1 && minY == -1) {
-      return null;
-    }
-
-    int size;
-    if (minX == -1) {
-      size = maxY - minY + 1;
-    } else {
+    if (minX != -1 && maxX != -1) {
       size = maxX - minX + 1;
+    } else if (minY != -1 && maxY != -1) {
+      size = maxY - minY + 1;
     }
 
-    int xOffset = maxX - size + 1;
-    int yOffset = maxY - size + 1;
+    if (minX != -1) {
+      xOffset = minX;
+    } else if (maxX != -1 && size != -1) {
+      xOffset = maxX - size + 1;
+    }
+
+    if (minY != -1) {
+      yOffset = minY;
+    } else if (maxY != -1 && size != -1) {
+      yOffset = maxY - size + 1;
+    }
+
+    if (size == -1 || xOffset == -1 || yOffset == -1) {
+      return null;
+    }
 
     return new MapInfo(size, xOffset, yOffset, minX, maxX, minY, maxY);
   }
